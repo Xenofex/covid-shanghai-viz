12a683797dd71d9e60a96a2071eeb684ea6ced86
王昕烨 <xinye.wang@foresight-soft.com>
2022-04-23 12:22:39 +0800

    更新4月22日数据，适应4月22日的新的语言解析

--- index.mjs
+++ index.mjs
@@ -2,11 +2,9 @@ import { JSDOM } from 'jsdom'
 import { readdir, readFile, writeFile } from 'fs/promises'
 import { existsSync } from 'fs'
 
-// const dataFile = 'docs/data.json'
-
-// const data = existsSync(dataFile) ? JSON.parse(await readFile(dataFile)) : {}
-
-const districtOpeningRegex = /(浦东新|黄浦|静安|徐汇|长宁|普陀|虹口|杨浦|宝山|闵行|嘉定|金山|松江|青浦|奉贤|崇明).*?(?:(\d+例)|无新增).*?确诊病例.*?(?:(\d+例)|无新增).*?无症状感染者/
+const districtRegex = /(浦东新|黄浦|静安|徐汇|长宁|普陀|虹口|杨浦|宝山|闵行|嘉定|金山|松江|青浦|奉贤|崇明).*(?:确诊病例|无症状感染者)/
+const symptomaticRegex = /(\d+例|无新增)[^，]*?确诊病例|确诊病例(\d+例)/
+const asymptomaticRegex = /((?<=新增)\d+例|无新增)[^，]*无症状感染者|无症状感染者(\d+例)/
 
 async function parseFile(path) {
   let date = path.match(/\d+月\d+日/)?.[0]
@@ -44,10 +42,17 @@ async function parseFile(path) {
       data[district.name] = district
     }
 
-    if (m = text.match(districtOpeningRegex)) {
+    if (m = text.match(districtRegex)) {
       setDistrict(m[1] + '区')
-      district.cases = parseInt(m[2], 10) || 0
-      district.asymptomaticCases = parseInt(m[3], 10) || 0
+
+      if (m = text.match(symptomaticRegex)) {
+        district.cases = parseInt(m[1] || m[2], 10) || 0
+      }
+
+      if (m = text.match(asymptomaticRegex)) {
+        district.asymptomaticCases = parseInt(m[1] || m[2], 10) || 0
+      }
+
       continue
     }
 
@@ -67,7 +72,7 @@ async function parseFile(path) {
 const files = await readdir('html')
 
 for (let file of files) {
-  await parseFile(file)
+  if (file.endsWith('.html')) {
+    await parseFile(file)
+  }
 }
-
-// await writeFile(dataFile, JSON.stringify(data))
